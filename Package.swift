8c199aea090a9ce5874fd6e2a7b9129bc335a853
Ethan Kay <elk941@gmail.com>
2016-09-25 18:35:37 -0500

    rename

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-  name: "SQLite.swift",
+  name: "SQLite",
   dependencies: [
     .Package(url: "https://github.com/carlbrown/CSQLite.git", majorVersion: 0)
   ]
