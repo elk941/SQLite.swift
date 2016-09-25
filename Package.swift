72a9c7040297dd812d0fbf06e068cfa7857e1d26
Ethan Kay <elk941@gmail.com>
2016-09-25 18:24:25 -0500

    more cleaning

--- Package.swift
+++ Package.swift
@@ -2,9 +2,6 @@ import PackageDescription
 
 let package = Package(
   name: "SQLite.swift",
-  targets: [
-    Target(name: "Core", dependencies: []),
-  ],
   dependencies: [
     .Package(url: "https://github.com/carlbrown/CSQLite.git", majorVersion: 0)
   ]
